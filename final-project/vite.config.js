a311f16785ae1582bb9f560cd47ac5c0f59ba06d
Esmith-Sdev <esmith897@ivytech.edu>
2025-09-29 18:57:37 -0400

    Route Changes

--- final-project/vite.config.js
+++ final-project/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/SDEV_255_Final_Project_Group_8/",
+  base: "/SDEV_255_Final_Project_Group_8",
   build: { outDir: "docs" },
   plugins: [react()],
 });
