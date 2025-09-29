3358a3fa91772204d5e0cba2001e553e264c4ae4
Esmith-Sdev <esmith897@ivytech.edu>
2025-09-29 18:30:06 -0400

    Route Changes

--- final-project/src/main.jsx
+++ final-project/src/main.jsx
@@ -8,11 +8,9 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./styles/index.css";
 import App from "./App";
 
-const base = "/SDEV_255_Final_Project_Group_8";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename={base}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
